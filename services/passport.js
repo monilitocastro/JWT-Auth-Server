28a72294e5e1fe62ab31073e347dc219021a11dc
Monilito Castro <mannycastro.epic@gmail.com>
2017-05-22 18:19:33 -0500

    LOGIN logic using JWT

--- services/passport.js
+++ services/passport.js
@@ -3,3 +3,19 @@ const User = require('../models/user');
 const passport = require('passport');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+const jwtOptions = {};
+
+const jwtLogin = new JwtStrategy( jwtOptions, function(payload, done){
+    User.findById( payload.sub, function(err, user){
+        if(err){
+            return done(err, false);
+        }
+
+        if(user){
+            done(null, user);
+        }else{
+            done(null, false);
+        }
+    });
+});
