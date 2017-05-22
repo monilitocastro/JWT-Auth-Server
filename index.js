29f25841cb297818de1b8c6e92cc499133735447
Monilito Castro <mannycastro.epic@gmail.com>
2017-05-22 14:05:01 -0500

    link ROUTER in index

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+
+const router = require('./router');
+
 const app = express();
 
 // app setup
 app.use( morgan('combined'));
 app.use(bodyParser.json({type:'*/*'}));
-
+router(app);
 
 // server setup
 
