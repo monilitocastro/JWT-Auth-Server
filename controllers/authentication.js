98d874dc54dfaf791b0d603a04d29cd024d9f17a
Monilito Castro <mannycastro.epic@gmail.com>
2017-05-22 15:57:45 -0500

    SIGNUP logic scaffold

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,17 +1,31 @@
-const User = require('../model/user');
+const User = require('../models/user');
 
 exports.signup = function(req, res, next){
     const email = req.body.email;
     const password = req.body.password;
-    
+
     // see if user with given email exists
+    User.findOne({ email: email }, function(err, existingUser){
+        if(err) return next(err);
 
     // if user with email exists return error
-
+        if(existingUser){
+            return res.status(422);
+        }
 
     // if userwith email DNE create and save user
+    const user = new User({
+        email: email,
+        password: password
+    });
 
+    user.save(function(err){
+        if(err) { return next(err);}
 
     // response to request indicating the user was created
+        res.json(user)
+    });
+
+    });
 
 }
