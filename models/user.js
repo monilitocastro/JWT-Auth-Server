de81f2fcef3f33574c5a0fda1d9ec612f1adad6f
Monilito Castro <mannycastro.epic@gmail.com>
2017-05-23 10:05:41 -0500

    CORRECTION bcrypt spelling

--- models/user.js
+++ models/user.js
@@ -37,7 +37,7 @@ userSchema.pre('save', function(next){
 });
 
 userSchema.methods.comparePassword = function(candidatePassword, callback){
-    bcrypt.comparePassword( candidatePassword, this.password, function(err, isMatch){
+    bcrypt.compare( candidatePassword, this.password, function(err, isMatch){
         if(err){
             return callback(err);
         }
